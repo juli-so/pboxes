e0ff2fc5ee169c43eae87650415dc3363be1da43
Gerald <gera2ld@163.com>
2015-08-01 10:01:17 +0800

    Remove Flight and Medoo dependencies

--- php/PBoxes.php
+++ php/PBoxes.php
@@ -1,16 +1,25 @@
 <?php
 class PBoxes {
 	private function __construct() {
-		$this -> ret = [
+		$this->ret = [
 			'code' => -1,
 			'data' => '我书读得少，你不要骗我。',
 		];
 	}
+
+  /**
+   * Get db connection, MUST be overridden
+   * @return a PDO object
+   */
+  protected static function pdo() {
+    throw 'Not implemented!'
+  }
+
 	private static $instance;
 	private $ret;
 	private function finish() {
     header('Content-Type: application/json');
-		die(json_encode($this -> ret));
+		die(json_encode($this->ret));
 	}
 
   private function _checkFreq() {
@@ -18,8 +27,8 @@ class PBoxes {
     $last_ts = isset($_SESSION['timestamp']) ? (int) $_SESSION['timestamp'] : 0;
     $cur_ts = $_SERVER['REQUEST_TIME'];
     if ($last_ts && $cur_ts - $last_ts < 3) {
-      $this -> ret['data'] = '发送过于频繁，请先休息一下~';
-      $this -> finish();
+      $this->ret['data'] = '发送过于频繁，请先休息一下~';
+      $this->finish();
     }
     $_SESSION['timestamp'] = $cur_ts;
   }
@@ -33,19 +42,10 @@ class PBoxes {
   }
 
   public function _get($url, $limit = 100) {
-    $db = Flight::db();
-    $res = $db -> select('pboxes', [
-      'id',
-      'x',
-      'y',
-      'color',
-      'text',
-      'thumbs',
-    ], [
-      'url' => $url,
-      'LIMIT' => $limit,
-      'ORDER' => 'id DESC',
-    ]);
+    $pdo = self::pdo();
+    $st = $pdo->prepare('SELECT id,x,y,color,text,thumbs FROM pboxes WHERE url=? ORDER BY id DESC LIMIT ?');
+    $st->execute([$url, $limit]);
+    $res = $st->fetchAll(PDO::FETCH_ASSOC);
     $list = [];
     foreach ($res as $r)
       $list[] = [
@@ -56,9 +56,9 @@ class PBoxes {
         'text' => $r['text'],
         'thumbs' => (int) $r['thumbs'],
       ];
-    $this -> ret['code'] = 0;
-    $this -> ret['data'] = &$list;
-    $this -> finish();
+    $this->ret['code'] = 0;
+    $this->ret['data'] = &$list;
+    $this->finish();
   }
 
   public function _add($url, $x, $y, $color, $text) {
@@ -66,37 +66,28 @@ class PBoxes {
     $x = number_format($x, 2, '.', ''); // percentage
     $y = number_format($y, 2, '.', ''); // pixels
     $color = (int) $color;
-    if ($x < 0 || $x > 100 || $y < 0 || mb_strlen($text) < 4) self::finish();
-    $db = Flight::db();
-    $r = $db -> insert('pboxes', [
-      'url' => $url,
-      'x' => $x,
-      'y' => $y,
-      'color' => $color,
-      'text' => $text,
-      'ip' => Flight::request() -> ip,
-    ]);
+    if ($x < 0 || $x > 100 || $y < 0 || mb_strlen($text) < 4) $this->finish();
+    $pdo = self::pdo();
+    $st = $pdo->prepare('INSERT INTO pboxes (url,x,y,color,text,ip) VALUES(?,?,?,?,?,?)');
+    $r = $st->execute([$url, $x, $y, $color, $text, $_SERVER['REMOTE_ADDR']]);
     if ($r) {
-      $this -> ret['code'] = 0;
-      $this -> ret['data'] = $r;
+      $this->ret['code'] = 0;
+      $this->ret['data'] = $r;
     } else {
-      $this -> ret['code'] = 1;
-      $this -> ret['data'] = '添加失败！';
-      // $db->error()[2];
+      $this->ret['code'] = 1;
+      $this->ret['data'] = '添加失败！';
+      // $st->errorInfo()[2];
     }
-    $this -> finish();
+    $this->finish();
   }
 
   public function _thumbUp($id) {
     $this->_checkFreq();
-    $db = Flight::db();
-    $r = $db -> update('pboxes', [
-      'thumbs[+]' => 1,
-    ], [
-      'id' => $id,
-    ]);
-    $this -> ret['code'] = 0;
-    $this -> ret['data'] = $r;
-    $this -> finish();
+    $pdo = self::pdo();
+    $st = $pdo->prepare('UPDATE pboxes SET thumbs=thumbs+1 WHERE id=?');
+    $r = $st->execute([$id]);
+    $this->ret['code'] = 0;
+    $this->ret['data'] = $r;
+    $this->finish();
   }
 }
