400e794a2cb7f8795efac443e8c01b43eb01bfa5
Gerald <gera2ld@163.com>
2015-08-01 09:29:07 +0800

    Remove dependency on Session

--- php/PBoxes.php
+++ php/PBoxes.php
@@ -1,100 +1,101 @@
 <?php
 class PBoxes {
-	private function __construct() {
-		$this -> ret = [
-			'code' => -1,
-			'data' => '我书读得少，你不要骗我。',
-		];
-	}
-	private static $instance;
-	private $ret;
-	private function finish() {
-		Flight::json($this -> ret);
-	}
-
-  private function _checkFreq() {
-		$last_ts = Session::get('timestamp');
-		$cur_ts = $_SERVER['REQUEST_TIME'];
-		if ($last_ts && $cur_ts - $last_ts < 3) {
-			$this -> ret['data'] = '发送过于频繁，请先休息一下~';
-			$this -> finish();
-		}
-		Session::set('timestamp', $cur_ts);
+  private function __construct() {
+    $this -> ret = [
+      'code' => -1,
+      'data' => '我书读得少，你不要骗我。',
+    ];
+  }
+  private static $instance;
+  private $ret;
+  private function finish() {
+    Flight::json($this -> ret);
   }
 
-	public static function __callStatic($name, $params) {
-		if (!isset(self::$instance)) {
-			$cls = __CLASS__;
-			self::$instance = new $cls;
-		}
-		call_user_func_array(array(self::$instance, '_' . $name), $params);
-	}
+  private function _checkFreq() {
+    if (session_status() !== PHP_SESSION_ACTIVE) session_start();
+    $last_ts = isset($_SESSION['timestamp']) ? (int) $_SESSION['timestamp'] : 0;
+    $cur_ts = $_SERVER['REQUEST_TIME'];
+    if ($last_ts && $cur_ts - $last_ts < 3) {
+      $this -> ret['data'] = '发送过于频繁，请先休息一下~';
+      $this -> finish();
+    }
+    $_SESSION['timestamp'] = $cur_ts;
+  }
 
-	public function _get($url, $limit = 100) {
-		$db = Flight::db();
-		$res = $db -> select('pboxes', [
-			'id',
-			'x',
-			'y',
-			'color',
-			'text',
-			'thumbs',
-		], [
-			'url' => $url,
-			'LIMIT' => $limit,
-			'ORDER' => 'id DESC',
-		]);
-		$list = [];
-		foreach ($res as $r)
-			$list[] = [
-				'id' => (int) $r['id'],
-				'x' => (float) $r['x'],
-				'y' => (int) $r['y'],
-				'color' => (int) $r['color'],
-				'text' => $r['text'],
-				'thumbs' => (int) $r['thumbs'],
-			];
-		$this -> ret['code'] = 0;
-		$this -> ret['data'] = &$list;
-		$this -> finish();
-	}
+  public static function __callStatic($name, $params) {
+    if (!isset(self::$instance)) {
+      $cls = __CLASS__;
+      self::$instance = new $cls;
+    }
+    call_user_func_array(array(self::$instance, '_' . $name), $params);
+  }
 
-	public function _add($url, $x, $y, $color, $text) {
+  public function _get($url, $limit = 100) {
+    $db = Flight::db();
+    $res = $db -> select('pboxes', [
+      'id',
+      'x',
+      'y',
+      'color',
+      'text',
+      'thumbs',
+    ], [
+      'url' => $url,
+      'LIMIT' => $limit,
+      'ORDER' => 'id DESC',
+    ]);
+    $list = [];
+    foreach ($res as $r)
+      $list[] = [
+        'id' => (int) $r['id'],
+        'x' => (float) $r['x'],
+        'y' => (int) $r['y'],
+        'color' => (int) $r['color'],
+        'text' => $r['text'],
+        'thumbs' => (int) $r['thumbs'],
+      ];
+    $this -> ret['code'] = 0;
+    $this -> ret['data'] = &$list;
+    $this -> finish();
+  }
+
+  public function _add($url, $x, $y, $color, $text) {
     $this->_checkFreq();
-		$x = number_format($x, 2, '.', '');	// percentage
-		$y = number_format($y, 2, '.', '');	// pixels
-		$color = (int) $color;
-		if ($x < 0 || $x > 100 || $y < 0 || mb_strlen($text) < 4) self::finish();
-		$db = Flight::db();
-		$r = $db -> insert('pboxes', [
-			'url' => $url,
-			'x' => $x,
-			'y' => $y,
-			'color' => $color,
-			'text' => $text,
-			'ip' => Flight::request() -> ip,
-		]);
-		if ($r) {
-			$this -> ret['code'] = 0;
-			$this -> ret['data'] = $r;
-		} else {
-			$this -> ret['code'] = 1;
-			$this -> ret['data'] = '添加失败！';
-			// $db->error()[2];
-		}
-		$this -> finish();
-	}
+    $x = number_format($x, 2, '.', ''); // percentage
+    $y = number_format($y, 2, '.', ''); // pixels
+    $color = (int) $color;
+    if ($x < 0 || $x > 100 || $y < 0 || mb_strlen($text) < 4) self::finish();
+    $db = Flight::db();
+    $r = $db -> insert('pboxes', [
+      'url' => $url,
+      'x' => $x,
+      'y' => $y,
+      'color' => $color,
+      'text' => $text,
+      'ip' => Flight::request() -> ip,
+    ]);
+    if ($r) {
+      $this -> ret['code'] = 0;
+      $this -> ret['data'] = $r;
+    } else {
+      $this -> ret['code'] = 1;
+      $this -> ret['data'] = '添加失败！';
+      // $db->error()[2];
+    }
+    $this -> finish();
+  }
 
-	public function _thumbUp($id) {
+  public function _thumbUp($id) {
     $this->_checkFreq();
-		$db = Flight::db();
-		$r = $db -> update('pboxes', [
-			'thumbs[+]' => 1,
-		], [
-			'id' => $id,
-		]);
-		$this -> ret['code'] = 0;
-		$this -> ret['data'] = $r;
-		$this -> finish();
-	}
+    $db = Flight::db();
+    $r = $db -> update('pboxes', [
+      'thumbs[+]' => 1,
+    ], [
+      'id' => $id,
+    ]);
+    $this -> ret['code'] = 0;
+    $this -> ret['data'] = $r;
+    $this -> finish();
+  }
 }
